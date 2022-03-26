340c5dd59f60c71a035408bc65bca2f822087698
ryuki1126 <uv.ryuki1126@gmail.com>
2022-03-26 16:50:34 +0900

    fixed

---
+++ components/materials/PokemonSuggest.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <vue-simple-suggest
+      class="w-60"
+      v-model="selected"
+      :list="japanesePokemonNames"
+      :filter-by-query="true">
+    </vue-simple-suggest>
+  </div>
+
+</template>
+
+<script>
+import VueSimpleSuggest from 'vue-simple-suggest'
+import 'vue-simple-suggest/dist/styles.css'
+import pokemonList from "@/assets/json/japanesePokemonNames.json"
+export default {
+  components: {
+    VueSimpleSuggest
+  },
+  data() {
+    return {
+      selected: null,
+      items: ["Anton", "Andrey", "Sasha", "Vladimir", "Dima"],
+      japanesePokemonNames: []
+    }
+  },
+  mounted() {
+    pokemonList.forEach(element => {
+      const pokemonName = element['name_ja']
+      this.japanesePokemonNames.push(pokemonName)
+    })
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,19 @@
 <template>
-  <Tutorial/>
+  <div>
+    hello
+    <PokemonSuggest />
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
 export default {
+  components: {
+    PokemonSuggest: () => import('~/components/materials/PokemonSuggest'),
+  },
   async fetch() {
-      const res = await axios.get('http://localhost:8000/api/test')
-      console.log(res)
-    },
+    const res = await axios.get('http://localhost:8000/api/test')
+    console.log(res)
+  },
 }
 </script>
