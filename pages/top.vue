637bc749f3f91e37e164f60230292e9183ef7370
ryuki1126 <uv.ryuki1126@gmail.com>
2022-03-31 22:31:14 +0900

    fixed

--- pages/top.vue
+++ pages/top.vue
@@ -1,6 +1,47 @@
 <template>
   <div>
-    top
+    <div class="flex items-center flex-col my-4">
+      <div class="w-2/5 h-40 my-2 card card-side bg-base-100 shadow-xl">
+        <figure><img src="https://api.lorem.space/image/movie?w=200&h=280" alt="Movie"></figure>
+        <div class="card-body">
+          <h2 class="card-title">New movie is released!</h2>
+          <p>Click the button to watch on Jetflix app.</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-warning">Watch</button>
+          </div>
+        </div>
+      </div>
+      <div class="w-2/5 h-40 my-2 card card-side bg-base-100 shadow-xl">
+        <figure><img src="https://api.lorem.space/image/movie?w=200&h=280" alt="Movie"></figure>
+        <div class="card-body">
+          <h2 class="card-title">New movie is released!</h2>
+          <p>Click the button to watch on Jetflix app.</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-warning">Watch</button>
+          </div>
+        </div>
+      </div>
+      <div class="w-2/5 h-40 my-2 card card-side bg-base-100 shadow-xl">
+        <figure><img src="https://api.lorem.space/image/movie?w=200&h=280" alt="Movie"></figure>
+        <div class="card-body">
+          <h2 class="card-title">New movie is released!</h2>
+          <p>Click the button to watch on Jetflix app.</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-warning">Watch</button>
+          </div>
+        </div>
+      </div>
+      <div class="w-2/5 h-40 my-2 card card-side bg-base-100 shadow-xl">
+        <figure><img src="https://api.lorem.space/image/movie?w=200&h=280" alt="Movie"></figure>
+        <div class="card-body">
+          <h2 class="card-title">New movie is released!</h2>
+          <p>Click the button to watch on Jetflix app.</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-warning">Watch</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
