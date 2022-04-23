94399aa8cac71f383a91eb60c2da97697ffb58bc
ryuki1126 <uv.ryuki1126@gmail.com>
2022-04-24 00:01:54 +0900

    ポケモン登録フォームの修正

--- components/materials/input/GeneralInput.vue
+++ components/materials/input/GeneralInput.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="flex items-center">
-    <label class="w-24" for="">ニックネーム</label>
-    <input type="text" class="w-60 border-2 p-2 ml-4">
+    <label class="w-24" for="">{{ label }}</label>
+    <input type="text" class="w-60 border-2 p-2 ml-4" :value="value">
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    label: { type: String },
+    value: { type: String }
+  }
+}
+</script>
--- pages/pokemon/new.vue
+++ pages/pokemon/new.vue
@@ -6,7 +6,15 @@
         <label class="w-24" for="">ポケモン名</label>
         <PokemonSuggestInput class="ml-4" />
       </div>
-      <GeneralInput />
+      <GeneralInput label="ニックネーム" :value="inputValue" />
+      <GeneralInput label="HP" />
+      <GeneralInput label="攻撃" />
+      <GeneralInput label="防御" />
+      <GeneralInput label="特攻" />
+      <GeneralInput label="特防" />
+      <GeneralInput label="素早さ" />
+      <GeneralInput label="備考" />
+      <button type="submit" class="btn btn-warning px-12" @click.prevent="insertData">登録</button>
     </form>
   </div>
 </template>
@@ -17,5 +25,15 @@ export default {
     PokemonSuggestInput: () => import('~/components/materials/input/PokemonSuggestInput.vue'),
     GeneralInput: () => import('~/components/materials/input/GeneralInput.vue')
   },
+  data() {
+    return {
+      inputValue: 'pokemon'
+    }
+  },
+  methods: {
+    insertData() {
+      alert(this.inputValue)
+    }
+  }
 }
 </script>
